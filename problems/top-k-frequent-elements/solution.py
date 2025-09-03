748e1e76a1ad0cfd4beed2513d90fa665f5b1db7
JeremyDevCode <jeremydevcode@gmail.com>
2025-09-02 20:58:47 -0500

    feat: resolve top k frequent elements

---
+++ problems/top-k-frequent-elements/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        counter = {}
+
+        for num in nums:
+            counter[num] = 1 + counter.get(num, 0)
+        
+        for key, val in counter.items():
+            heapq.heappush(heap, (-val, key))
+        
+        result = []
+
+        while len(result) < k:
+            result.append(heapq.heappop(heap)[1])
+        
+        return result
